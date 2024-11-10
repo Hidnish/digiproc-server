fa9131c2410e44d346ed6ea28662caa5334db7a8
HidNish <hideonishimura@live.it>
2024-11-10 21:46:37 +0100

    Set up Rating
    - Module
    - Controller
    - Service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { ProductsModule } from './products/products.module';
+import { RatingModule } from './rating/rating.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { ProductsModule } from './products/products.module';
     UsersModule,
     AuthModule,
     ProductsModule,
+    RatingModule,
   ],
   controllers: [],
   providers: [],
