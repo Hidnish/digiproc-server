572f9dc4357448c1f1da0525981c2ac3d0a443f8
HidNish <hideonishimura@live.it>
2024-11-10 21:28:54 +0100

    Add login functionality
    - Allow user to login
    - Pass JWT with cookie to NextJS server

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Res, UseGuards } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { Response } from 'express';
+import { LocalAuthGuard } from './guards/local-auth.guard';
+import { CurrentUser } from './decorators/current-user.decorator';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  login(@CurrentUser() user: User, @Res({ passthrough: true }) res: Response) {
+    return this.authService.login(user, res);
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,43 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { User } from '@prisma/client';
 import * as bcrypt from 'bcrypt';
+import { Response } from 'express';
+import ms from 'ms';
 import { UsersService } from 'src/users/users.service';
+import { TokenPayload } from './types/token-payload.interface';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UsersService) {}
+  constructor(
+    private readonly userService: UsersService,
+    private readonly configService: ConfigService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async login(user: User, res: Response) {
+    const expirationDate = new Date();
+
+    expirationDate.setMilliseconds(
+      expirationDate.getMilliseconds() +
+        ms(this.configService.getOrThrow<string>('JWT_EXPIRATION')),
+    );
+
+    const tokenPayload: TokenPayload = {
+      userId: user.id,
+    };
+
+    const token = this.jwtService.sign(tokenPayload);
+
+    res.cookie('Auth', token, {
+      secure: false, // set to true in PROD
+      httpOnly: true,
+      expires: expirationDate,
+    });
+
+    return { tokenPayload };
+  }
 
   async verifyUser(email: string, password: string) {
     try {
