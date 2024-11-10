cae805d4a2cab6f77105011a3093d5ba4ba6501e
HidNish <hideonishimura@live.it>
2024-11-10 21:09:36 +0100

    Set up endpoint to create new user
    - encrypt password
    - store user in DB

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseInterceptors } from '@nestjs/common';
+import { NoFilesInterceptor } from '@nestjs/platform-express';
+import { CreateUserDto } from './dto/create-user';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+
+  @Post()
+  @UseInterceptors(NoFilesInterceptor())
+  createUser(@Body() request: CreateUserDto) {
+    return this.userService.createUser(request);
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnprocessableEntityException } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { CreateUserDto } from './dto/create-user';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async createUser({ email, password }: CreateUserDto) {
+    try {
+      const encryptedPassword = await bcrypt.hash(password, 10);
+
+      const user = await this.prismaService.user.create({
+        data: {
+          email,
+          password: encryptedPassword,
+        },
+        select: {
+          email: true,
+          id: true,
+        },
+      });
+
+      return user;
+    } catch (err) {
+      if (err.code === 'P2002') {
+        throw new UnprocessableEntityException('Email already exists');
+      }
+      throw err;
+    }
+  }
+}
